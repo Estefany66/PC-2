3b5fdd7523d6e29d9aeaf596feed375af305894a
Estefany Calatayud <estefany_calatayud@usmp.pe>
2021-04-27 18:58:23 -0500

    nosotros

---
+++ pc-2/Models/NosotrosViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace pc_2.Models
+{
+    public class NosotrosViewModel
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+        public string Direccion { get; set; }
+
+        
+    }
+}
